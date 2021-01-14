ed0c39e77c23029ed8283bc6450b739914431b60
Lucaspers <dunderdykaren@hotmail.com>
2021-01-14 13:26:04 +0100

    installed passport for secure api

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     protected $fillable = [
         'name', 
@@ -20,7 +22,7 @@ class User extends Authenticatable
         'password', 
         'remember_token',
     ];
-
+    
     public function orders()
     {
         return $this->hasMany(Order::class);
