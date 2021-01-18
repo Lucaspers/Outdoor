6195bd9785305059368c107016d6ec263e48c062
Lucaspers <dunderdykaren@hotmail.com>
2021-01-18 19:55:56 +0100

    added vue's and need error fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+//.sass('resources/sass/app.scss', 'public/css')
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+.js('resources/js/bootstrap.js', 'public/js')
+.postCss('resources/css/app.css', 'public/css', [
+        
+]).vue();
